b07b5320c79cd8f638e228b3420297a0152ad0ce
Muhammad Umair Raza <umair.raza101@gmail.com>
2022-03-03 16:21:53 +0500

    Repace chatwoot to one chat

--- spec/jobs/internal/check_new_versions_job_spec.rb
+++ spec/jobs/internal/check_new_versions_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Internal::CheckNewVersionsJob, type: :job do
   subject(:job) { described_class.perform_now }
 
-  it 'updates the latest chatwoot version in redis' do
+  it 'updates the latest OneChat version in redis' do
     version = '1.1.1'
     allow(Rails.env).to receive(:production?).and_return(true)
     allow(ChatwootHub).to receive(:latest_version).and_return(version)
