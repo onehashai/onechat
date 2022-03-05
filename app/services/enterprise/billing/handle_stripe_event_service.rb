59269139e5601ef7f766468cc2d575f3b9f4aad9
Umair Raza <Muhammad Umair Raza>
2022-03-05 19:46:44 +0500

    handle subscription updating and cancellation

--- app/services/enterprise/billing/handle_stripe_event_service.rb
+++ app/services/enterprise/billing/handle_stripe_event_service.rb
@@ -9,7 +9,21 @@ class Enterprise::Billing::HandleStripeEventService
         account.account_billing_subscriptions.create!(billing_product_price: subscription_price, subscription_stripe_id: subscription.id,
                                                       current_period_end: Time.at(subscription.current_period_end).utc.to_datetime)
         account.set_limits_for_account subscription_price
-      else
+      when "customer.subscription.updated"
+        subscription = event.data.object
+        account = Account.where("custom_attributes->>'stripe_customer_id' = ?", subscription.customer).first
+        subscription_price = Enterprise::BillingProductPrice.find_by(price_stripe_id: subscription.plan.id)
+        active_subscription = account.account_billing_subscriptions.where(subscription_stripe_id: subscription.id)
+        active_subscription.update(current_period_end: Time.at(subscription.current_period_end)) if active_subscription
+        account.set_limits_for_account subscription_price
+      when "customer.subscription.deleted"
+        subscription = event.data.object
+        account = Account.where("custom_attributes->>'stripe_customer_id' = ?", subscription.customer).first
+        subscription_price = Enterprise::BillingProductPrice.find_by(price_stripe_id: subscription.plan.id)
+        active_subscription = account.account_billing_subscriptions.where(subscription_stripe_id: subscription.id)
+        active_subscription.update(current_period_end: Time.at(subscription.current_period_end), cancelled_at: Time.current) if active_subscription
+        account.set_limits_for_account subscription_price
+        else
         Rails.logger.debug { "Unhandled event type: #{event.type}" }
       end
     end
